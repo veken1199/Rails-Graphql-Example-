d1e39ddca1ebcf64b423ca7021e76eef5bd50d29
veken1199 <super_veko@yahoo.com>
2019-10-06 20:41:23 -0400

    Added todo list Item db model and its rspec

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_10_07_003611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.boolean "done"
+    t.bigint "todo_list_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["todo_list_id"], name: "index_items_on_todo_list_id"
+  end
+
+  create_table "todo_lists", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "items", "todo_lists"
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -68,7 +68,7 @@ RSpec.configure do |config|
   #start by truncating all the tables but then use the faster transaction strategy the rest of the time.
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
-    DatabaseCleaner.strategy = :transactions
+    DatabaseCleaner.strategy = :transaction
   end
 
  # start the transaction strategy as examples are run
